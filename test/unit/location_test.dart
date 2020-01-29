2555f3be604be9f336d4dd12859d47855877cac4
MariannaOLE <marianna.olefyrenko@gmail.com>
2020-01-29 16:38:06 +0800

    Consuming Web Services
    Learn about Futures, async/await, testing and integrating web services.

--- test/unit/location_test.dart
+++ test/unit/location_test.dart
@@ -1,22 +1,17 @@
-import 'dart:convert';
 import 'package:test/test.dart';
 import '../../lib/models/location.dart';
 
 void main() {
-  test('test location deserialization', () {
-    const locationJSON =
-        '{ "name": "Arashiyama Bamboo Grove", "url": "https://cdn-images-1.medium.com/max/2000/1*vdJuSUKWl_SA9Lp-32ebnA.jpeg", "facts": [{ "title": "Summary", "text": "This bamboo grove is on the outskirts of Kyoto." }] }';
+  test('test /locations and /locations/:id', () async {
+    final locations = await Location.fetchAll();
+    for (var location in locations) {
+      expect(location.name, hasLength(greaterThan(0)));
+      expect(location.url, hasLength(greaterThan(0)));
 
-    final locationMap = json.decode(locationJSON) as Map<String, dynamic>;
-
-    expect("Arashiyama Bamboo Grove", equals(locationMap['name']));
-
-    final location = Location.fromJson(locationMap);
-
-    expect(location.name, equals(locationMap['name']));
-    expect(location.url, equals(locationMap['url']));
-
-    expect(location.facts[0].title, matches(locationMap['facts'][0]['title']));
-    expect(location.facts[0].text, matches(locationMap['facts'][0]['text']));
+      final fetchedLocation = await Location.fetchByID(location.id);
+      expect(fetchedLocation.name, equals(location.name));
+      expect(fetchedLocation.url, equals(location.url));
+      expect(fetchedLocation.facts, hasLength(location.facts.length));
+    }
   });
 }
